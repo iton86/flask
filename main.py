da98e2c45a969cad1c4279bc06ec129680f8f082
iton86 <iton86@abv.bg>
2023-04-03 12:46:45 +0300

    Changing dummy API with date API - still dummy data

--- main.py
+++ main.py
@@ -1,52 +1,18 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+data = {
+    "2022-01-01": "Data for January 1st, 2022",
+    "2022-01-02": "Data for January 2nd, 2022",
+    "2022-01-03": "Data for January 3rd, 2022"
+}
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"Received: {name}")
-
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
+@app.route('/data/<date>', methods=['GET'])
+def get_data_by_date(date):
+    if date in data:
+        return jsonify({date: data[date]})
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
-
-
-@app.route('/')
-def index():
-    # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
-
+        return jsonify({"error": "Data not found for date {}".format(date)}), 404
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
